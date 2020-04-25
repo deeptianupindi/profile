c4802233d0ca8288b76d6841f929de56975be0ad
lavendroid <5360592+deepictorial@users.noreply.github.com>
2020-04-25 10:03:55 -0700

    phase 2

--- js/game.js
+++ js/game.js
@@ -1,11 +1,27 @@
-function showCoords(event) {
-  var x = event.clientX-320;
-  var y = event.clientY-20;
-  document.getElementById("textDisplay").innerHTML = "X is : "+x+", Y is : "+y;
-}
+var msg = document.getElementById('msg');
+var msgboard = document.getElementsByClassName('msgboard')[0];
+var parent = document.getElementsByClassName('parentdiv')[0];
 
 function activatePlane() {
-  var plane = document.getElementById("fly");
+  var plane = document.createElement('img');
+  plane.src = "images/plane.png";
+  plane.id = 'fly';
   plane.className = 'plane-start';
-  setTimeout(function() { plane.className = 'plane-hide' }, 11000);
+  parent.append(plane);
+  setTimeout(function() { plane.parentNode.removeChild(plane) }, 6000);
+  msg.innerHTML = "Welcome to Cleveland!";
+}
+
+function activateAsia() {
+
+}
+
+function activateBeach() {
+  var chair = document.createElement('img');
+  chair.src = "images/icons/chair.png";
+  chair.id = 'chair';
+  chair.className = 'msgboard chair-slide';
+  msgboard.append(chair);
+  msg.innerHTML = "Walk along the banks of Lake Eerie, where families, students, everyone else get to relax and unwind.";
+  setTimeout(function() { chair.parentNode.removeChild(chair) }, 6000);
 }
