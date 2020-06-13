96a8cfb0b8f170ee7400d0fb65cd272fdf346316
lavendroid <5360592+deepictorial@users.noreply.github.com>
2020-06-13 11:23:29 -0700

    build

---
+++ build/precache-manifest.8de8504adb2e9567e639ec2c02027666.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38e38623c05e656b8bd697ebb48d0ab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7959e1eee75fd3868f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "911abee0e2330e4c46d5",
+    "url": "/static/css/main.3ee5c93e.chunk.css"
+  },
+  {
+    "revision": "4b7959e1eee75fd3868f",
+    "url": "/static/js/2.34b7a091.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.34b7a091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911abee0e2330e4c46d5",
+    "url": "/static/js/main.8afc2e9e.chunk.js"
+  },
+  {
+    "revision": "150a317df26e0eaff2e3",
+    "url": "/static/js/runtime-main.a4c88018.js"
+  },
+  {
+    "revision": "89c098133cf7c7c6b9712b67c28722bf",
+    "url": "/static/media/american.89c09813.svg"
+  },
+  {
+    "revision": "88e38f589e6c9fa2898c91be97c774e5",
+    "url": "/static/media/mphs.88e38f58.png"
+  },
+  {
+    "revision": "0023448811c76be3368fa8f0f01b9eeb",
+    "url": "/static/media/shutterfly.00234488.svg"
+  },
+  {
+    "revision": "35099a2e9824cabfd293f6c631a88f31",
+    "url": "/static/media/wood.35099a2e.png"
+  }
+]);
